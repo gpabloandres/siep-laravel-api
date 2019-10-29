add671ee77d6fda09f037f16ad09c2e649876fc4
MaTiUs77 <matius77@gmail.com>
2019-10-29 11:38:50 -0300

    NroSorteo por Route
    Permite definir numero de sorteo via route

--- app/Http/Controllers/Api/Saneo/routes.php
+++ app/Http/Controllers/Api/Saneo/routes.php
@@ -4,7 +4,7 @@ Route::prefix('v1')->group(function () {
     Route::prefix('saneo')->group(function () {
         Route::get('repitencia', 'Api\Saneo\v1\SaneoRepitencia@start');
         Route::get('edad', 'Api\Saneo\v1\SaneoEdad@start');
-        Route::get('sorteo/{ciclo}/{nivel_servicio}', 'Api\Saneo\v1\SaneoSorteo@start');
+        Route::get('sorteo/{ciclo}/{nivel_servicio}/{nro_sorteo}', 'Api\Saneo\v1\SaneoSorteo@start');
     });
 });
 
