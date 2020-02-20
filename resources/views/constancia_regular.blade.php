1a8615e9565268e6de43159a1bbf6bc87e3ef1c3
Pablo <gpabloandres@gmail.com>
2020-02-20 10:44:34 -0300

    Nueva actualización de la leyenda.

--- resources/views/constancia_regular.blade.php
+++ resources/views/constancia_regular.blade.php
@@ -50,7 +50,7 @@
                         @endif
                     </td>
                     <td>
-                        <div style="text-align:right; line-height: 150px; font-size: 10px">“2020 - Año del General Manuel Belgrano”</div>
+                        <div style="text-align:right; line-height: 150px; font-size: 9px">“2020 - Año del General Manuel Belgrano”</div>
                     </td>
                 </tr>
             </table>
